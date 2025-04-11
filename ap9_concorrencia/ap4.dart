c6b5c55de34a3b61a5e4e77a1931f5b3d3330b4c
Alexandre de Melo <84542748+Siiki-89@users.noreply.github.com>
2025-04-11 15:02:27 -0300

    Codigo comentado

--- ap9_concorrencia/ap4.dart
+++ ap9_concorrencia/ap4.dart
@@ -1,9 +1,11 @@
 void main() async{
   print('Inicializando a busca dos dados');
   print('Buscando dados, aguarde');
+  //Chamando metodo buscar dados
   await buscarDados();
   print('Dados buscados com sucesso');
 }
 Future<void> buscarDados () async{
+  //Simular a busca
   await Future.delayed(Duration(seconds: 3));
 }
