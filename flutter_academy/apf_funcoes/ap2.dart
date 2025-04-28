2b6dc25a1813f26d4cd09267fefcdade9b5203a6
Alexandre de Melo <84542748+Siiki-89@users.noreply.github.com>
2025-04-28 16:11:43 -0300

    comentando ap2

--- flutter_academy/apf_funcoes/ap2.dart
+++ flutter_academy/apf_funcoes/ap2.dart
@@ -14,31 +14,35 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  int contador = 0;
+  int contador = 0; //Para marcar a rodada
   final random = Random();
-  late int botaoCorreto;
+  late int botaoCorreto; //Registrar o botao sorteado aleatoriamente
   String resultado = '';
-  Color colorResultado = Colors.transparent;
-  bool jogoEncerrado = false;
+  Color colorResultado = Colors.transparent; //Armazenar a cor do resultado
+  bool jogoEncerrado = false; //Encerrar o jogo
 
   @override
   void initState() {
     super.initState();
-    botaoCorreto = random.nextInt(3) + 1;
+    botaoCorreto = random.nextInt(3) + 1; //Numero aleatorio de 1 a 3
   }
 
+  //Metodo para verificar se acertou ou errou o botão sorteado
   void verificarBotao(int botaoSelecionado) {
-    if (jogoEncerrado) return;
+    if (jogoEncerrado) return; //Encerra caso true
     setState(() {
       if (botaoCorreto == botaoSelecionado) {
+        //Venceu
         resultado = 'Você ganhou!';
         colorResultado = Colors.green;
-        jogoEncerrado = true;
+        jogoEncerrado = true; //Finaliza o jogo
       } else if (contador >= 2) {
+        //Perdeu
         resultado = 'Você perdeu!';
         colorResultado = Colors.red;
         jogoEncerrado = true;
       } else {
+        //Errou
         resultado = 'Você errou!';
         colorResultado = Colors.red;
         contador++;
@@ -54,22 +58,37 @@ class _MyAppState extends State<MyApp> {
         body: Center(
           child: Column(
             children: [
+              //Botão 1
               TextButton(
                 style: TextButton.styleFrom(backgroundColor: Colors.blue),
-                onPressed: () => verificarBotao(1),
+                onPressed:
+                    () => verificarBotao(
+                      1,
+                    ), //Chamando o metodo e passando o botão selecionado
                 child: Text('Botão 1', style: TextStyle(color: Colors.white)),
               ),
               TextButton(
+                //Botão 2
                 style: TextButton.styleFrom(backgroundColor: Colors.blue),
-                onPressed: () => verificarBotao(2),
+                onPressed:
+                    () => verificarBotao(
+                      2,
+                    ), //Chamando o metodo e passando o botão selecionado
                 child: Text('Botão 2', style: TextStyle(color: Colors.white)),
               ),
               TextButton(
+                //Botão 3
                 style: TextButton.styleFrom(backgroundColor: Colors.blue),
-                onPressed: () => verificarBotao(3),
+                onPressed:
+                    () => verificarBotao(
+                      3,
+                    ), //Chamando o metodo e passando o botão selecionado
                 child: Text('Botão 3', style: TextStyle(color: Colors.white)),
               ),
-              Text(resultado, style: TextStyle(color: colorResultado)),
+              Text(
+                resultado,
+                style: TextStyle(color: colorResultado),
+              ), //Texto para retornar o resultado
             ],
           ),
         ),
